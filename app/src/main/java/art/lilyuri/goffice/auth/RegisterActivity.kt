c2506c609c2d5d409fdfe0eb4628dd30216a70d4
2tle <iam@2tle.io>
2022-11-03 21:27:47 +0900

    Feat: add-fix Register Activity, fix Menifest

--- app/src/main/java/art/lilyuri/goffice/auth/RegisterActivity.kt
+++ app/src/main/java/art/lilyuri/goffice/auth/RegisterActivity.kt
@@ -3,21 +3,25 @@ package art.lilyuri.goffice.auth
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import art.lilyuri.goffice.databinding.ActivityBoardlistBinding
 import art.lilyuri.goffice.databinding.ActivityLoginBinding
+import art.lilyuri.goffice.databinding.ActivityRegisterBinding
 
-class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLogin.setOnClickListener {
+        binding.btnRegister.setOnClickListener {
             val email: String = binding.etEmail.text.toString()
             val password: String = binding.etPassword.text.toString()
-            //Request
+            val company: String = binding.etCompany.text.toString()
+            val department: String = binding.etDepartment.text.toString()
+            val position: String = binding.etPosition.text.toString()
 
+            //Request
         }
 
 
