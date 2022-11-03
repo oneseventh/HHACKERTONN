308a5ce8220cdf31459c82d6071403a5a8af7ad2
2tle <iam@2tle.io>
2022-11-03 22:57:10 +0900

    Feat: add login/register logic

--- app/src/main/java/art/lilyuri/goffice/auth/LoginActivity.kt
+++ app/src/main/java/art/lilyuri/goffice/auth/LoginActivity.kt
@@ -1,10 +1,19 @@
 package art.lilyuri.goffice.auth
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import art.lilyuri.goffice.MainActivity
+import art.lilyuri.goffice.data.LoginBody
+import art.lilyuri.goffice.data.TokenData
 import art.lilyuri.goffice.databinding.ActivityBoardlistBinding
 import art.lilyuri.goffice.databinding.ActivityLoginBinding
+import art.lilyuri.goffice.retrofit.RetrofitAPI
+import art.lilyuri.goffice.sharedpreferences.SharedPreferences
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -17,6 +26,23 @@ class LoginActivity : AppCompatActivity() {
             val email: String = binding.etEmail.text.toString()
             val password: String = binding.etPassword.text.toString()
             //Request
+//            val data = LoginBody(email, password)
+//            val call = RetrofitAPI.getApiService().login(data)
+//            call.enqueue(object : Callback<TokenData> {
+//                override fun onResponse(call: Call<TokenData>, response: Response<TokenData>) {
+//                    if(response.isSuccessful) {
+//                        SharedPreferences.prefs.setString("token", response.body()!!.token)
+//                        goMain()
+//                    } else {
+//                        showErrorMsg("로그인 실패")
+//                    }
+//
+//                }
+//
+//                override fun onFailure(call: Call<TokenData>, t: Throwable) {
+//                    showErrorMsg("로그인 실패")
+//                }
+//            } )
 
         }
 
@@ -26,5 +52,10 @@ class LoginActivity : AppCompatActivity() {
     fun showErrorMsg(msg: String) {
         binding.tvError.text = msg
         binding.tvError.visibility = View.VISIBLE
+    }
+
+    fun goMain() {
+        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+        finish()
     }
 }
